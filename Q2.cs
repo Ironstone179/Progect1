bf6ef784d956ef4ee1342ac5dce19c4d13788c80
Ironstone179 <muzhele@mail.ru>
2016-10-18 19:12:14 +0300

    Add files via upload

---
+++ Q2.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+{
+ 
+public class Program
+ 
+{
+ 
+public static void Main(string[] args)
+ 	
+{
+ 		
+int y,x;
+ 	
+x=10;
+ 	
+while (x<=99)
+ 	
+{
+  		
+y=x%10;
+  	
+if (y==3)
+  	
+{
+  		
+Console.WriteLine(x);
+  		
+}
+  			
+x=x+1;
+  		
+}
+  		
+Console.ReadKey();
+ 	
+}
+ 	
+}
+
+}
---
+++ Q3.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+{
+ 
+public class Program
+ 
+{
+ 	
+public static void Main(string[] args)
+ 		
+{
+ 		
+double y, z;
+ 	
+int x;
+ 	
+Console.WriteLine("Input n:");
+ 	
+x = Convert.ToInt32(Console.ReadLine());
+ 	
+z=System.Math.Pow(0.5, x);
+ 		
+y=0.5*(z-1)/(0.5-1);
+ 		
+Console.WriteLine(y);
+  	
+Console.ReadKey();
+ 	
+}
+ 
+}
+
+}
